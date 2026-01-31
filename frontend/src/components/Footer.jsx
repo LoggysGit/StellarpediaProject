761185133bfe3333f04f87ae263ca1d6c4406ad4
LoggysGit <albertmetsler3008@gmail.com>
2026-01-31 13:29:43 +0500

    Developing

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Link } from 'react-router';
+
+import '../assets/variables.css'
+import './styles/Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+
+    </footer>
+  );
+}
+
+export default Footer
--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -5,6 +5,7 @@ import { motion, useScroll, useTransform } from 'framer-motion';
 import BGParallaxElement from '../components/BGParallaxElement';
 import FeatureCarousel from '../components/FeatureCarousel';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 // === Style ===
 import './styles/Common.css';
 import './styles/WelcomePage.css';
@@ -29,8 +30,59 @@ function WelcomePage() {
           </div>
           </BGParallaxElement>
           <p className='text-primary-dark heading'>What do we have</p>
-          <FeatureCarousel></FeatureCarousel>
+          <FeatureCarousel speed='3.0'></FeatureCarousel>
+          <FeatureCarousel speed='-3.0'></FeatureCarousel>
+          <BGParallaxElement>
+          <div className='qa-box'>
+            <p className='text-primary-light heading'>We open for everyone!</p>
+            <p className='text-primary-light'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button>Start Voyage!</button>
+          </div>
+          </BGParallaxElement>
+          <p className='text-primary-dark heading'>Stellarpedia X</p>
+          <div id='subscription-box'>
+              <div className='subscription-offer'>
+                <p id='sub-label'>Free</p>
+                <p id='sub-price'>$0/Month</p>
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                </ul>
+                <ul id='crossed-list'>
+                  <li>Feature 4</li>
+                  <li>Feature 5</li>
+                </ul>
+                <button id='selected-plan-btn'>Your Plan</button>
+              </div>
+              <div className='subscription-offer'>
+                <p id='sub-label'>Montly</p>
+                <p id='sub-price'>$5/Month</p>
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                  <li>Feature 4</li>
+                  <li>Feature 5</li>
+                </ul>
+                <button>Details</button>
+              </div>
+              <div className='subscription-offer'>
+                <p id='discount-badge'>-20%</p>
+                <p id='sub-label'>Annual</p>
+                <p id='sub-price'>$4/Month</p>
+                <ul>
+                  <li>Feature 1</li>
+                  <li>Feature 2</li>
+                  <li>Feature 3</li>
+                  <li>Feature 4</li>
+                  <li>Feature 5</li>
+                </ul>
+                <button>Details</button>
+              </div>
+          </div>
         </div>
+        <Footer/>
     </div>
   );
 }
